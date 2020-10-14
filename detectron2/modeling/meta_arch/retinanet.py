d1e903f1056291203dfe417495b26f782ff759fa
yy115 <yy115@duke.edu>
2020-10-14 16:56:57 +0200

    add print to check output

--- detectron2/modeling/meta_arch/retinanet.py
+++ detectron2/modeling/meta_arch/retinanet.py
@@ -149,7 +149,9 @@ class RetinaNet(nn.Module):
         images = self.preprocess_image(batched_inputs)
         features = self.backbone(images.tensor)
         print("--------START OUTPUT OF BACKBONE----------------")
-        print(features)
+        for name, tensor in features.items():
+            print(name)
+            print(tensor.size())
         print("----------END OUTPUT OF BACKBONE----------------")
         features = [features[f] for f in self.in_features]
 
