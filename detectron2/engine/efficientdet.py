88e1e2c164ff344e203d5e4d6d60846d623a8e86
yy115 <yy115@duke.edu>
2020-10-18 16:11:37 +0200

    add ResizeWithPadding and modify Predictor

--- detectron2/engine/efficientdet.py
+++ detectron2/engine/efficientdet.py
@@ -15,6 +15,7 @@ import os
 import sys
 from collections import OrderedDict
 import torch
+import numpy as np
 from fvcore.common.file_io import PathManager
 from fvcore.nn.precise_bn import get_bn_modules
 from torch.nn.parallel import DistributedDataParallel
@@ -39,119 +40,50 @@ from detectron2.utils.collect_env import collect_env_info
 from detectron2.utils.env import seed_all_rng
 from detectron2.utils.events import CommonMetricPrinter, JSONWriter, TensorboardXWriter
 from detectron2.utils.logger import setup_logger
+from detectron2.engine.defaults import default_argument_parser, default_setup, DefaultTrainer, DefaultPredictor
+
+from detectron2.data.transforms.augmentation import Augmentation
+from detectron2.data.transforms.transform import ResizeTransform
+from PIL import Image
 
 from . import hooks
 from .train_loop import SimpleTrainer
 
-__all__ = ["default_argument_parser", "default_setup", "DefaultPredictor", "DefaultTrainer"]
+__all__ = ["ResizeWithPadding", "Predictor", "Trainer"]
 
 
-def default_argument_parser(epilog=None):
-    """
-    Create a parser with some common arguments used by detectron2 users.
+class ResizeWithPadding(Augmentation):
+    """ Resize image to a fixed target size"""
 
-    Args:
-        epilog (str): epilog passed to ArgumentParser describing the usage.
+    def __init__(self, shape, interp=Image.BILINEAR):
+        """
+        Args:
+            shape: int
+            interp: PIL interpolation method
+        """
+        self.shape = shape
 
-    Returns:
-        argparse.ArgumentParser:
-    """
-    parser = argparse.ArgumentParser(
-        epilog=epilog
-        or f"""
-Examples:
+    def get_transform(self, image):
+        height, width, _ = image.shape
+        if height > width:
+            scale = self.shape / height
+            resized_height = self.shape
+            resized_width = int(width * scale)
+        else:
+            scale = self.shape / width
+            resized_height = int(height * scale)
+            resized_width = self.shape
 
-Run on single machine:
-    $ {sys.argv[0]} --num-gpus 8 --config-file cfg.yaml MODEL.WEIGHTS /path/to/weight.pth
-
-Run on multiple machines:
-    (machine0)$ {sys.argv[0]} --machine-rank 0 --num-machines 2 --dist-url <URL> [--other-flags]
-    (machine1)$ {sys.argv[0]} --machine-rank 1 --num-machines 2 --dist-url <URL> [--other-flags]
-""",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-    parser.add_argument("--config-file", default="", metavar="FILE", help="path to config file")
-    parser.add_argument(
-        "--resume",
-        action="store_true",
-        help="whether to attempt to resume from the checkpoint directory",
-    )
-    parser.add_argument("--eval-only", action="store_true", help="perform evaluation only")
-    parser.add_argument("--num-gpus", type=int, default=1, help="number of gpus *per machine*")
-    parser.add_argument("--num-machines", type=int, default=1, help="total number of machines")
-    parser.add_argument(
-        "--machine-rank", type=int, default=0, help="the rank of this machine (unique per machine)"
-    )
-
-    # PyTorch still may leave orphan processes in multi-gpu training.
-    # Therefore we use a deterministic way to obtain port,
-    # so that users are aware of orphan processes by seeing the port occupied.
-    port = 2 ** 15 + 2 ** 14 + hash(os.getuid() if sys.platform != "win32" else 1) % 2 ** 14
-    parser.add_argument(
-        "--dist-url",
-        default="tcp://127.0.0.1:{}".format(port),
-        help="initialization URL for pytorch distributed backend. See "
-        "https://pytorch.org/docs/stable/distributed.html for details.",
-    )
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
-    return parser
-
-
-def default_setup(cfg, args):
-    """
-    Perform some basic common setups at the beginning of a job, including:
-
-    1. Set up the detectron2 logger
-    2. Log basic information about environment, cmdline arguments, and config
-    3. Backup the config to the output directory
-
-    Args:
-        cfg (CfgNode): the full config to be used
-        args (argparse.NameSpace): the command line arguments to be logged
-    """
-    output_dir = cfg.OUTPUT_DIR
-    if comm.is_main_process() and output_dir:
-        PathManager.mkdirs(output_dir)
-
-    rank = comm.get_rank()
-    setup_logger(output_dir, distributed_rank=rank, name="fvcore")
-    logger = setup_logger(output_dir, distributed_rank=rank)
-
-    logger.info("Rank of current process: {}. World size: {}".format(rank, comm.get_world_size()))
-    logger.info("Environment info:\n" + collect_env_info())
-
-    logger.info("Command line arguments: " + str(args))
-    if hasattr(args, "config_file") and args.config_file != "":
-        logger.info(
-            "Contents of args.config_file={}:\n{}".format(
-                args.config_file, PathManager.open(args.config_file, "r").read()
-            )
+        scaled = ResizeTransform(
+            height, width, resized_height, resized_width, self.interp
         )
+        scaled_and_padded = np.zeros((self.shape, self.shape, 3))
+        scaled_and_padded[0:resized_height, 0:resized_width] = scaled
 
-    logger.info("Running with full config:\n{}".format(cfg))
-    if comm.is_main_process() and output_dir:
-        # Note: some of our scripts may expect the existence of
-        # config.yaml in output directory
-        path = os.path.join(output_dir, "config.yaml")
-        with PathManager.open(path, "w") as f:
-            f.write(cfg.dump())
-        logger.info("Full config saved to {}".format(path))
-
-    # make sure each worker has a different, yet deterministic seed if specified
-    seed_all_rng(None if cfg.SEED < 0 else cfg.SEED + rank)
-
-    # cudnn benchmark has large overhead. It shouldn't be used considering the small size of
-    # typical validation set.
-    if not (hasattr(args, "eval_only") and args.eval_only):
-        torch.backends.cudnn.benchmark = cfg.CUDNN_BENCHMARK
+        return torch.from_numpy(scaled_and_padded).to(torch.float32)
 
 
-class DefaultPredictor:
+class Predictor:
     """
     Create a simple end-to-end predictor with the given config that runs on
     single device for a single input image.
@@ -187,12 +119,7 @@ class DefaultPredictor:
         checkpointer = DetectionCheckpointer(self.model)
         checkpointer.load(cfg.MODEL.WEIGHTS)
 
-        # TODO: to use efficientnet backbone, this must be replaced
-        #  by the Resizer in efficientdet/dataset.py. For example,
-        #  by define a new class ResizeWithPadding(Augmentation)
-
-        # TODO: for a first step, try force resize to 512x512
-        self.aug = T.Resize(512)
+        self.aug = ResizeWithPadding(cfg.INPUT.MIN_SIZE_TEST)
         # self.aug = T.ResizeShortestEdge(
         #     [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
         # )
@@ -224,7 +151,7 @@ class DefaultPredictor:
             return predictions
 
 
-class DefaultTrainer(SimpleTrainer):
+class Trainer(DefaultTrainer):
     """
     A trainer with default training logic.
     It is a subclass of :class:`SimpleTrainer` and instantiates everything needed from the
