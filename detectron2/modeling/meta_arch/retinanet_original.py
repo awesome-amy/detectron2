2c53d7d2279bc0e4e342b0d6dbbc4968c272281c
yy115 <yy115@duke.edu>
2020-10-19 20:30:36 +0200

    rewrite postprocess for using efficientdet backbone

--- detectron2/modeling/meta_arch/retinanet_original.py
+++ detectron2/modeling/meta_arch/retinanet_original.py
@@ -18,9 +18,7 @@ from ..anchor_generator import build_anchor_generator
 from ..backbone import build_backbone
 from ..box_regression import Box2BoxTransform
 from ..matcher import Matcher
-# from ..postprocessing import detector_postprocess
-from detectron2.utils.memory import retry_if_cuda_oom
-from detectron2.layers import paste_masks_in_image
+from ..postprocessing import detector_postprocess
 from .build import META_ARCH_REGISTRY
 
 __all__ = ["RetinaNet"]
@@ -36,77 +34,6 @@ def permute_to_N_HWA_K(tensor, K):
     tensor = tensor.permute(0, 3, 4, 1, 2)
     tensor = tensor.reshape(N, -1, K)  # Size=(N,HWA,K)
     return tensor
-
-
-def detector_postprocess(results, output_height, output_width, mask_threshold=0.5):
-    """
-    Resize the output instances.
-    The input images are often resized when entering an object detector.
-    As a result, we often need the outputs of the detector in a different
-    resolution from its inputs.
-
-    This function will resize the raw outputs of an R-CNN detector
-    to produce outputs according to the desired output resolution.
-
-    Args:
-        results (Instances): the raw outputs from the detector.
-            `results.image_size` contains the input image resolution the detector sees.
-            This object might be modified in-place.
-        output_height, output_width: the desired output resolution.
-
-    Returns:
-        Instances: the resized output from the model, based on the output resolution
-    """
-
-    # Converts integer tensors to float temporaries
-    #   to ensure true division is performed when
-    #   computing scale_x and scale_y.
-    if isinstance(output_width, torch.Tensor):
-        output_width_tmp = output_width.float()
-    else:
-        output_width_tmp = output_width
-
-    if isinstance(output_height, torch.Tensor):
-        output_height_tmp = output_height.float()
-    else:
-        output_height_tmp = output_height
-
-    if output_height_tmp > output_width_tmp:
-        results_height = results.image_size[0]
-        results_width = results.image_size[1] - int(results.image_size[0] * output_width_tmp / output_height_tmp)
-    else:
-        results_height = results.image_size[0] - int(results.image_size[1] * output_height_tmp / output_width_tmp)
-        results_width = results.image_size[1]
-
-    scale_x, scale_y = (
-        output_width_tmp / results_width,
-        output_height_tmp / results_height,
-    )
-    results = Instances((output_height, output_width), **results.get_fields())
-
-    if results.has("pred_boxes"):
-        output_boxes = results.pred_boxes
-    elif results.has("proposal_boxes"):
-        output_boxes = results.proposal_boxes
-
-    output_boxes.scale(scale_x, scale_y)
-    output_boxes.clip(results.image_size)
-
-    results = results[output_boxes.nonempty()]
-
-    if results.has("pred_masks"):
-        results.pred_masks = retry_if_cuda_oom(paste_masks_in_image)(
-            results.pred_masks[:, 0, :, :],  # N, 1, M, M
-            results.pred_boxes,
-            results.image_size,
-            threshold=mask_threshold,
-        )
-
-    if results.has("pred_keypoints"):
-        results.pred_keypoints[:, :, 0] *= scale_x
-        results.pred_keypoints[:, :, 1] *= scale_y
-
-    return results
 
 
 @META_ARCH_REGISTRY.register()
