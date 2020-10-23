30beec5fb5638e098bbb8eaa0e3033799531ccba
yy115 <yy115@duke.edu>
2020-10-23 20:54:32 +0200

    check image prepocessing

--- detectron2/engine/efficientdet.py
+++ detectron2/engine/efficientdet.py
@@ -174,7 +174,10 @@ class Predictor:
                 original_image = original_image[:, :, ::-1]
             height, width = original_image.shape[:2]
             image = self.aug.get_transform(original_image).apply_image(original_image)
+            print(image.size())
+            # TODO: what does this tranpose do?
             image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+            print(image.size())
 
             inputs = {"image": image, "height": height, "width": width}
             predictions = self.model([inputs])[0]
