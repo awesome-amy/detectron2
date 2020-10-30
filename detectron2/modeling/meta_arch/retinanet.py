737510aabce2484fd4be852c1351431afe3f32c0
yy115 <yy115@duke.edu>
2020-10-30 20:18:25 +0100

    debug bbox when mask is on

--- detectron2/modeling/meta_arch/retinanet.py
+++ detectron2/modeling/meta_arch/retinanet.py
@@ -287,6 +287,17 @@ class RetinaNet(nn.Module):
             # results = self.inference(anchors, pred_logits, pred_anchor_deltas, images.image_sizes)
             processed_results = []
             results, _ = self.mask(images, features_dict, detections)
+
+            # TODO: here detections' and results' .pred_boxes may have diverged
+            for results_per_image, detections_per_image in zip(results, detections):
+                print(results_per_image.pred_classes)
+                print(detections_per_image.pred_boxes)
+                assert results_per_image.pred_classes == detections_per_image.pred_classes
+                print(results_per_image.pred_boxes)
+                print(detections_per_image.pred_boxes)
+                assert results_per_image.pred_boxes == detections_per_image.pred_boxes
+
+
             for results_per_image, input_per_image, image_size in zip(
                 results, batched_inputs, images.image_sizes
             ):
